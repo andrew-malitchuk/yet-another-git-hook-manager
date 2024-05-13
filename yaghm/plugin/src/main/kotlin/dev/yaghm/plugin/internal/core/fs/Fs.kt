d201c76e5e69191281cd6296acb65b7669d48da5
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-05-13 21:23:28 +0300

    feat(YAGHM-2): Add some methods for file works

--- yaghm/plugin/src/main/kotlin/dev/yaghm/plugin/internal/core/fs/Fs.kt
+++ yaghm/plugin/src/main/kotlin/dev/yaghm/plugin/internal/core/fs/Fs.kt
@@ -1,6 +1,10 @@
 package dev.yaghm.plugin.internal.core.fs
 
+import java.io.BufferedWriter
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.FileWriter
 import java.io.IOException
 
 class Fs {
@@ -24,6 +28,49 @@ class Fs {
             e.printStackTrace()
         }
         return file
+    }
+
+    fun cloneFile(sourceFilePath: String, destinationFilePath: String) {
+        val sourceFile = File(sourceFilePath)
+        val destinationFile = File(destinationFilePath)
+
+        require(sourceFile.exists())
+        require(destinationFile.exists())
+
+        FileInputStream(sourceFile).use { inputStream ->
+            FileOutputStream(destinationFile).use { outputStream ->
+                inputStream.copyTo(outputStream)
+            }
+        }
+    }
+
+    fun appendTextToFile(filePath: String, textToAdd: String) {
+        val file = File(filePath)
+
+        require(file.exists())
+        require(textToAdd.isNotEmpty())
+
+        try {
+            val writer = BufferedWriter(FileWriter(file, true))
+            writer.append(textToAdd)
+            writer.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun clearFile(filePath: String) {
+        val file = File(filePath)
+
+        require(file.exists())
+
+        try {
+            val writer = FileWriter(file)
+            writer.write("")
+            writer.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
 }
