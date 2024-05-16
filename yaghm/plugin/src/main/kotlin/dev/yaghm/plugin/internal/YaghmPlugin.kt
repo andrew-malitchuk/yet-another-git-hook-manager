5c96e353dbfb213c1774b2227587eb068256c98f
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-05-16 21:53:06 +0300

    feat(YAGHM-2): Play-around with task

--- yaghm/plugin/src/main/kotlin/dev/yaghm/plugin/internal/YaghmPlugin.kt
+++ yaghm/plugin/src/main/kotlin/dev/yaghm/plugin/internal/YaghmPlugin.kt
@@ -11,17 +11,30 @@ class YaghmPlugin : Plugin<Project> {
         with(project) {
             val yaghmConfig = yaghmConfig()
 
-
             project.tasks.apply {
-                register(INSTALL_GIT_HOOK, InstallGitHookTask::class.java) { task ->
-
+                register(Tasks.INSTALL_GIT_HOOK, InstallGitHookTask::class.java) { task ->
+                    yaghmConfig.gitHook.apply {
+                        task.group = Group.GROUP_GIT_HOOK
+                        task.description = Description.DESCRIPTION_GIT_HOOK
+                        task.gitHookConfig.set(this)
+                    }
                 }
             }
         }
     }
 
     companion object {
-        const val INSTALL_GIT_HOOK = "installGitHook"
+        object Tasks {
+            const val INSTALL_GIT_HOOK = "installGitHook"
+        }
+
+        object Group {
+            const val GROUP_GIT_HOOK = "installGitHook"
+        }
+
+        object Description {
+            const val DESCRIPTION_GIT_HOOK = "installGitHook"
+        }
     }
 
 }
--- yaghm/plugin/src/main/kotlin/dev/yaghm/plugin/internal/tasks/InstallGitHookTask.kt
+++ yaghm/plugin/src/main/kotlin/dev/yaghm/plugin/internal/tasks/InstallGitHookTask.kt
@@ -1,7 +1,11 @@
 package dev.yaghm.plugin.internal.tasks
 
+import dev.yaghm.plugin.internal.config.GitHookConfig
 import org.gradle.api.DefaultTask
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.options.Option
 
 abstract class InstallGitHookTask : DefaultTask() {
     init {
@@ -9,9 +13,26 @@ abstract class InstallGitHookTask : DefaultTask() {
         description = "Install specific GitHook"
     }
 
+    @get:Input
+    abstract val gitHookConfig: Property<GitHookConfig>
+
+    @Option(description = Descriptions.DEBUG_DESCRIPTION, option = Params.DEBUG_OUTPUT)
+    @get:Input
+    var debugOutput: Boolean = false
+
     @TaskAction
     fun action() {
 
+    }
+
+    companion object {
+        object Params {
+            const val DEBUG_OUTPUT = "debugOutput"
+        }
+
+        object Descriptions {
+            const val DEBUG_DESCRIPTION = "debugOutput"
+        }
     }
 
 }
