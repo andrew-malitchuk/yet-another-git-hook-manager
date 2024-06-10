c14f926f93f60803c5411550d7d7a04d49c744c2
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-06-10 21:03:46 +0300

    vn: 0.0.1-a.2

--- yaghm/yaghm/src/main/kotlin/dev/yaghm/plugin/internal/core/dsl/bash/Bash.kt
+++ yaghm/yaghm/src/main/kotlin/dev/yaghm/plugin/internal/core/dsl/bash/Bash.kt
@@ -13,6 +13,7 @@ import dev.yaghm.plugin.internal.model.Command
  * @property content The content of the Bash script as a string.
  * @return [String] The complete Bash script content with shebang (if provided) and script commands.
  */
+class Bash {
 
     var shebang: Shebang? = null
     var content: String = ""
@@ -24,6 +25,7 @@ ${shebang?.interpreter}
 $content
 """.trimIndent().trim()
         }
+}
 
 /**
  * Configures the shebang (interpreter line) for this Bash script using a lambda expression.
